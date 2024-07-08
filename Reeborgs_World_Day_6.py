c811a45da5f708793e900ac2283ea3d17a75eebe
Anzie <anzieghimire@gmail.com>
2024-07-08 12:26:17 -0500

    Create Reeborgs_World_Day_6.py
    Reeborg's World code.
    World: Maze

---
+++ Reeborgs_World_Day_6.py
@@ -0,0 +1,14 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
+      #This code only works on Reeborg's World!
